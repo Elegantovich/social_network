38ef8ee23a7d6a2d27c42f1fa4a61074181a9e2f
Elegantovich <kh.msm@yandex.ru>
2022-05-29 19:44:21 +0300

    add docker

--- SocialNetwork/api/serializers.py
+++ SocialNetwork/api/serializers.py
@@ -1,42 +1,79 @@
 from rest_framework import serializers
-from .models import User, Favorite
 
-class CommonSubscribed(metaclass=serializers.SerializerMetaclass):
+from .models import Favorite, Post, User
 
-    is_Favorite = serializers.SerializerMethodField()
+
+class IsFavorite(metaclass=serializers.SerializerMetaclass):
+    """
+    Set favorite status for main serializer.
+    """
+    is_favorite = serializers.SerializerMethodField()
 
     def get_is_favorite(self, obj):
         request = self.context.get('request')
         if request.user.is_anonymous:
             return False
         if Favorite.objects.filter(
-                user=request.user, favorites__id=obj.id).exists():
+                user=request.user, post=obj).exists():
             return True
         else:
             return False
 
 
 class UserSerializer(serializers.ModelSerializer):
-
+    """
+    Set serializer for User model.
+    """
     class Meta:
         model = User
         fields = ('id', 'username', 'email', 'first_name',
                   'last_name', 'password')
-        write_only_fields = ('password',)
+        read_only_fields = ('id',)
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+
+class PostCreateSerializer(serializers.ModelSerializer):
+    """
+    Set serializer for create objects of Post model.
+    """
+    author = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Post
+        fields = '__all__'
         read_only_fields = ('id',)
 
 
-class PostSerializer(serializers.ModelSerializer, CommonSubscribed):
+class PostSerializer(serializers.ModelSerializer, IsFavorite):
+    """
+    Set serializer for get objects of Post model.
+    """
+    author = UserSerializer(read_only=True)
 
     class Meta:
-        model = User
+        model = Post
         fields = '__all__'
         read_only_fields = ('id', 'is_favorite')
 
 
-class FavoriteSerializer(serializers.ModelSerializer, CommonSubscribed):
-
+class FavoriteSerializer(serializers.ModelSerializer):
+    """
+    Set serializer for Favorite model.
+    """
     class Meta:
         model = User
         fields = '__all__'
         read_only_fields = '__all__'
+
+
+class FavoriteListSerializer(serializers.ModelSerializer):
+    """
+    Set serializer for Favorite model.
+    """
+    post = PostSerializer()
+
+    class Meta:
+        model = Favorite
+        exclude = ('id', 'user')
--- SocialNetwork/api/views.py
+++ SocialNetwork/api/views.py
@@ -1,45 +1,107 @@
 from http import HTTPStatus
-from rest_framework import viewsets
-from .models import User, Post, Favorite
-from .serializers import UserSerializer, PostSerializer, FavoriteSerializer
+
 from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import AccessToken
+
+from .models import Favorite, Post, User
+from .serializers import (FavoriteListSerializer, FavoriteSerializer,
+                          PostCreateSerializer, PostSerializer, UserSerializer)
 
 
 class UserViewSet(viewsets.ModelViewSet):
-
+    """
+    Set viewset for User model.
+    """
+    permission_classes = [AllowAny]
     serializer_class = UserSerializer
     queryset = User.objects.all()
 
 
 class PostViewSet(viewsets.ModelViewSet):
-
+    """
+    Set viewset for Post model.
+    """
     serializer_class = PostSerializer
     queryset = Post.objects.all()
 
-
     def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+        serializer.save(author=self.request.user)
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return PostCreateSerializer
+        else:
+            return PostSerializer
 
 
 class FavoriteViewSet(viewsets.ModelViewSet):
-
+    """
+    Set viewset for Favorite model.
+    """
     serializer_class = FavoriteSerializer
     queryset = Favorite.objects.all()
     model = Favorite
 
     def create(self, request, *args, **kwargs):
-        post_id = int(self.kwargs['post_id'])
+        post_id = int(self.kwargs.get('post_id'))
         post = get_object_or_404(Post, id=post_id)
+        if post.author == request.user:
+            response = {'response': 'You do not add in favorite your posts!'}
+            return Response(response, HTTPStatus.NO_CONTENT)
+        if self.model.objects.filter(user=request.user, post=post).exists():
+            response = {'response': 'object is exists!'}
+            return Response(response, HTTPStatus.NO_CONTENT)
+        serialozers = PostCreateSerializer(post)
         self.model.objects.create(
             user=request.user, post=post)
-        return Response(HTTPStatus.CREATED)
+        return Response(serialozers.data, HTTPStatus.CREATED)
 
     def delete(self, request, *args, **kwargs):
-        post_id = self.kwargs['post_id']
+        post_id = self.kwargs.get('post_id')
         user_id = request.user.id
         object = get_object_or_404(
             self.model, user__id=user_id, post__id=post_id)
         object.delete()
-        return Response(HTTPStatus.NO_CONTENT)
+        response = {'response': 'object was deleted!'}
+        return Response(response, HTTPStatus.NO_CONTENT)
 
+
+class FavoriteListViewSet(viewsets.ModelViewSet):
+    """
+    Set viewset for Favorite model.
+    """
+    serializer_class = FavoriteListSerializer
+    queryset = Favorite.objects.all()
+    model = Favorite
+
+    def get_queryset(self):
+        return self.model.objects.filter(user=self.request.user)
+
+
+def get_token(user):
+    """
+    Create jwt token for login is app.
+    """
+    access = AccessToken.for_user(user)
+    return {'access': str(access)}
+
+
+class RecieveToken(APIView):
+    """
+    Recieve jwt token for login is app.
+    """
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        try:
+            username = request.data['username']
+            password = request.data['password']
+        except KeyError:
+            return Response(HTTPStatus.NOT_FOUND)
+        user = get_object_or_404(User, username=username, password=password)
+        response = {'auth_token': get_token(user)}
+        return Response(response, HTTPStatus.OK)
